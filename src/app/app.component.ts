540b38fe01111e51113bb59d5d14d6c8d3bdf266
Lorena Costa <lorenacosta.ufu@gmail.com>
2020-04-30 19:52:13 -0300

    I18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'site';
+export class AppComponent{
+
+  constructor(private titleService: Title){
+    // TODO: traduzir titulo
+    this.titleService.setTitle('Lorena Costa | Developer')
+  }
 }
