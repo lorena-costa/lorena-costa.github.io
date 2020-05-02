65c8af909b08eb0828dcc988578f7425f8316bde
Lorena Costa <lorenacosta.ufu@gmail.com>
2020-05-02 14:48:08 -0300

    Cont. Contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      _replyto: [null, [Validators.required,Validators.email]],
+      message: [null, Validators.required]
+    });
+
+  }
+  // TODO: Verificar maneira de impedir melhor spam
+  send() {
+    let url = 'https://formspree.io/lorenacosta.ufu@gmail.com'
+    this.http.post(url, this.form.value).subscribe(() => {
+      // TODO: Alertar que mensagem foi enviada com sucesso
+    })
+  }
+}
